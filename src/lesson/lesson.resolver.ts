d3b60d2d8d413b6fd864c98b19497db775abec32
Juan Pablo Tyrlik <juan.pablo.tyrlik@gmail.com>
2020-12-12 20:40:34 -0300

    Create and get Lessons using GraphQL

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,15 +1,27 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
+import { LessonService } from './lesson.service';
+import { Lesson } from './lesson.entity';
+import { CreateLessonInput } from './lesson.input';
 
 @Resolver((of) => LessonType)
 export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
+
   @Query((returns) => LessonType)
-  lesson(): LessonType {
-    return {
-      id: 'asdh%ashd',
-      name: 'Physics Class',
-      startDate: new Date().toUTCString(),
-      endDate: new Date().toISOString(),
-    };
+  getLessonById(@Args('id') id: string): Promise<Lesson> {
+    return this.lessonService.getLessonById(id);
+  }
+
+  @Query((returns) => [LessonType])
+  getLessons(): Promise<Lesson[]> {
+    return this.lessonService.getLessons();
+  }
+
+  @Mutation((returns) => LessonType)
+  createLesson(
+    @Args('createLessonInput') createLessonInput: CreateLessonInput,
+  ): Promise<Lesson> {
+    return this.lessonService.createLesson(createLessonInput);
   }
 }
