93b3c923b2b16025c44634b3d9069801e7b5becb
Juan Pablo Tyrlik <juan.pablo.tyrlik@gmail.com>
2020-12-12 22:16:03 -0300

    Assign and Retrieve Students to and from a Lesson

--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -4,6 +4,7 @@ import { v4 as uuid } from 'uuid';
 import { Repository } from 'typeorm';
 import { Lesson } from './lesson.entity';
 import { CreateLessonInput } from './create-lesson.input';
+import { AssignStudentsToLessonInput } from './assign-students-to-lesson.input';
 
 @Injectable()
 export class LessonService {
@@ -12,12 +13,13 @@ export class LessonService {
   ) {}
 
   async createLesson(createLessonInput: CreateLessonInput): Promise<Lesson> {
-    const { name, startDate, endDate } = createLessonInput;
+    const { name, startDate, endDate, students } = createLessonInput;
     const lesson = this.lessonRepository.create({
       id: uuid(),
       name,
       startDate,
       endDate,
+      students,
     });
     return this.lessonRepository.save(lesson);
   }
@@ -28,5 +30,14 @@ export class LessonService {
 
   async getLessons(): Promise<Lesson[]> {
     return this.lessonRepository.find();
+  }
+
+  async assignStudentsToLesson(
+    assignStudentsToLessonInput: AssignStudentsToLessonInput,
+  ): Promise<Lesson> {
+    const { lessonId, studentIds } = assignStudentsToLessonInput;
+    const lesson = await this.lessonRepository.findOne({ id: lessonId });
+    lesson.students = [...lesson.students, ...studentIds];
+    return this.lessonRepository.save(lesson);
   }
 }
